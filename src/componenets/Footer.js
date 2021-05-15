7a4f67e9bf22b777899d5f17061e206f52e3a63f
Ibrahim <ibrahim.s.amin@gmail.com>
2021-05-15 19:56:18 +1000

    routing completed
    tutorial completed

--- src/componenets/Footer.js
+++ src/componenets/Footer.js
@@ -1,5 +1,15 @@
+import { Link } from 'react-router-dom';
+
 const Footer = () => {
-   return <div>Footer</div>;
+   return (
+      <footer>
+         <p>Copyright &copy; 2021</p>
+
+         <h3>
+            <Link to='/about'>About</Link>
+         </h3>
+      </footer>
+   );
 };
 
 export default Footer;
